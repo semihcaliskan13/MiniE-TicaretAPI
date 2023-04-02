3505332a0bd4f97637c98f3fd5d4fc86cce30c6e
semihcaliskan13 <118068918+semihcaliskan13@users.noreply.github.com>
2023-04-02 22:26:00 +0300

    Update Program.cs
    Remove comment lines.

--- Kripto/Kripto/Program.cs
+++ Kripto/Kripto/Program.cs
@@ -1,8 +1,8 @@
-﻿string alfabe = "abcdefghijklmnopqrstuvwxyz";//26 harf hepsini kendi sırası kadar geriye götürecek. abz-zya
+﻿string alfabe = "abcdefghijklmnopqrstuvwxyz";
 string input = "povzhvhvmwznvhhztvdrgsxfiivmghvhhrlmrmqhlmulinzgerzgsrhvcznkov";
 string input2 = "4812x42ux36012274uu63230x6700y99yx4uvu15v91w89y499u659414zu29yz4";
 string output = "";
-//kensdisi kadar eksiği demek 26-kendisi kadar fazlası demek. 1-24 23 ileri götürecek. 2-25
+
 
 
 foreach (var item in input2)
